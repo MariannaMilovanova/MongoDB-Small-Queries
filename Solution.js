d885137a8d78639377dc9fe20cfd18c63192ce71
MariannaMilovanova <mmilovanova@kse.org.ua>
2017-05-28 17:18:14 +0300

    Add the file with the solution to the homework

---
+++ Solution.js
@@ -0,0 +1,39 @@
+//first task
+db.score.find({
+    "scores": {
+        $elemMatch: {
+            "score": {
+                $gt: 87,
+                $lt: 93
+            }
+        }
+    }
+}).pretty();
+
+//second tast
+db.score.aggregate({
+    $unwind: "$scores"
+}, {
+    $match: {
+        "scores.type": "exam",
+        "scores.score": {
+            $gt: 90
+        }
+    }
+}).pretty();
+
+//third task
+db.grades.find({
+    "name": "Dusti Lemmond"
+}).forEach(function(student) {
+    db.grades.update({
+        _id: student._id
+    }, {
+        $set: {
+            "accepted": true
+        }
+    });
+});
+db.grades.find({
+    "name": "Dusti Lemmond"
+}).pretty();
